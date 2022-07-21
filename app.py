697b90e8d151d042d63a4809e7f1f390d4e79f4a
Diego Marins <diegomarins33@gmail.com>
2022-07-20 21:15:18 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from calcipv4 import CalcIPv4
+
+# DIGITE O IP E O PREFIXO OU ADICIONE O PARÂMETRO MÁSCARA SE PREFERIR.
+calc_ipv4 = CalcIPv4(ip='192.168.0.1', prefixo=24)
+
+print(f'IP: {calc_ipv4.ip}')
+print(f'Máscara: {calc_ipv4.mascara}')
+print(f'Rede: {calc_ipv4.rede}')
+print(f'Broadcast: {calc_ipv4.broadcast}')
+print(f'Prefixo: {calc_ipv4.prefixo}')
+print(f'Número de IPS da rede: {calc_ipv4.numero_ips}')
---
+++ readme.md.txt
@@ -0,0 +1,2 @@
+Um calculador de rede IPV4, proposto pelo professor: 
+Otávio Miranda do curso "Python, Do Básico ao Avançado" (Udemy)
